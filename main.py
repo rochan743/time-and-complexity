a13942f1a5fac203672bbf369249500a01232323
Rochan743 <udaya.urvr@gmail.com>
2024-09-22 17:26:36 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+def caluculator1(n):
+    return n*(n+1)/2
+#print(caluculator1(6))
+
+def caluculator2(n):
+    sum = 0
+    for i in range(1,n+1):
+     sum+=1
+    return sum
+print(caluculator2(5))
+    
+
+def caluculator3(n):
+    sum=0
+    for i in range(1,n+1):
+        sum+=1
+        return sum
+print(caluculator3(7))           
